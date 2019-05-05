2ba4b1c7aeb322c390239f2cda3d13e3e02abc15
kerollos <kerolloz@yahoo.com>
2019-05-06 01:56:49 +0200

    add operations to problems route

--- src/routes/problems.ts
+++ src/routes/problems.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import * as problemController from '../controllers/problemController';
 
 const router = express.Router();
 
+router.get('/', );
+router.get('/:id', );
 
-
+// Make sure that the user is authenticated and is Problem-setter
+router.post('/', );
+router.put('/:id', );
+router.delete('/:id', );
 
 export default router;
