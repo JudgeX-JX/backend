0933fbc79a6cf59c4b219129a0a892e5796c9615
kerollos <kerolloz@yahoo.com>
2019-05-06 01:54:49 +0200

    add submission route

--- src/routes/submissions.ts
+++ src/routes/submissions.ts
@@ -2,7 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/', );
+router.get('/:id', );
 
+// Make sure that the user is authenticated
+router.post('/', );
 
+// and is Admin
+router.put('/:id', );
+router.delete('/:id', );
 
 export default router;
