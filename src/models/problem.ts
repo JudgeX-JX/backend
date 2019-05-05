3e7aaedc1d77a7439919476287022a5c104a6d22
kerollos <kerolloz@yahoo.com>
2019-05-06 01:56:24 +0200

    implement problem model

--- src/models/problem.ts
+++ src/models/problem.ts
@@ -1,2 +1,49 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+import { enumToArray } from '../utils/enumToArray';
 
-import { enumToArray } from '../utils/enumToArray';
+enum difficulty {
+  EASY,
+  MEDIUM,
+  HARD
+}
+
+const problemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50
+  },
+  timeLimit: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10
+  },
+  memoryLimit: {
+    type: Number,
+    required: true,
+    min: 5,
+    max: 500
+  },
+  tags: {
+    type: [String]
+  },
+  difficulty: {
+    type: String,
+    required: true
+  }
+});
+
+export const Problem = mongoose.model("Problem", problemSchema);
+
+export function validateProblem(problem: any) {
+  const schema = {
+    name: Joi.string().required().min(1).max(50),
+    timeLimit: Joi.number().required().min(1).max(10),
+    memoryLimit: Joi.number().required().min(5).max(500),
+    tags: Joi.array(),
+    difficulty: Joi.string().required().valid([...enumToArray(difficulty)])
+  }
+}
