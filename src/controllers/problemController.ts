8225e1b1a285bbf85afb09c492a8e945b0f41a16
kerollos <kerolloz@yahoo.com>
2019-05-06 01:56:01 +0200

    create problem controller

---
+++ src/controllers/problemController.ts
@@ -0,0 +1,23 @@
+import { Problem, validateProblem } from '../models/problem';
+
+// CRUD operations for a problem
+
+export function getProblems(req: any, res: any) {
+
+}
+
+export function getProblemWithId(req: any, res: any) {
+
+}
+
+export function createProblem(req: any, res: any) {
+
+}
+
+export function updateProblem(req: any, res: any) {
+
+}
+
+export function deleteProblem(req: any, res: any) {
+
+}
