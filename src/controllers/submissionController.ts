302f5f8186b3261301317a67cb72013a32881fcd
kerollos <kerolloz@yahoo.com>
2019-05-06 21:56:20 +0200

    add basic functions for submission controller

---
+++ src/controllers/submissionController.ts
@@ -0,0 +1,24 @@
+import { Submission, validateSubmission } from '../models/submission';
+import { Request, Response } from 'express';
+
+// CRUD operations
+
+export function getAll(req: Request, res: Response) {
+
+}
+
+export function getWithId(req: Request, res: Response) {
+
+}
+
+export function create(req: Request, res: Response) {
+
+}
+
+export function updateWithId(req: Request, res: Response) {
+
+}
+
+export function deleteWithId(req: Request, res: Response) {
+
+}
