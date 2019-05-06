75968de46f9cb6c2a77cdfac5b77b153c37d7fe9
kerollos <kerolloz@yahoo.com>
2019-05-06 21:59:48 +0200

    check user role for problems route

--- src/routes/problems.ts
+++ src/routes/problems.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import * as problemController from '../controllers/problemController';
+import { authenticate } from '../middlewares/authentication';
+import { authorize } from '../middlewares/authorization';
+import { Roles } from '../models/user';
 
 const router = express.Router();
 
-router.get('/', );
-router.get('/:id', );
+router.get('/', problemController.getAll);
+router.get('/:id', problemController.getWithId);
 
-// Make sure that the user is authenticated and is Problem-setter
-router.post('/', );
-router.put('/:id', );
-router.delete('/:id', );
+router.use(authenticate);
+router.use(authorize([Roles.PROBLEM_SETTER, Roles.ADMIN]));
+
+router.post('/', problemController.create);
+router.put('/:id', problemController.updateWithId);
+router.delete('/:id', problemController.deleteWithId);
 
 export default router;
