9fab1f280b324ace908c4939c0ea44317292c765
kerollos <kerolloz@yahoo.com>
2019-05-06 21:55:46 +0200

    add user role to authentication token

--- src/models/user.ts
+++ src/models/user.ts
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  const token = jwt.sign({ _id: this._id, role: this.role }, config.get('jwtPrivateKey'));
   return token;
 }
 
