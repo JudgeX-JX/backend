0e35c91f9d74daa9341e5341280824a1fd7c7a3f
kerollos <kerolloz@yahoo.com>
2019-05-06 21:59:18 +0200

    check for user Role for contest route operations

--- src/routes/contests.ts
+++ src/routes/contests.ts
@@ -1,8 +1,19 @@
 import express from 'express';
+import * as contestController from '../controllers/contestController';
+import { authenticate } from '../middlewares/authentication';
+import { authorize } from '../middlewares/authorization';
+import { Roles } from '../models/user';
 
 const router = express.Router();
 
+router.get('/', contestController.getAll);
+router.get('/:id', contestController.getWithId);
 
+router.use(authenticate);
+router.use(authorize([Roles.ADMIN]));
 
+router.post('/',  contestController.create);
+router.put('/:id', contestController.updateWithId);
+router.delete('/:id', contestController.deleteWithId);
 
 export default router;
