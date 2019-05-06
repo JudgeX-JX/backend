73a9e4b524ea1be8840e54a2417c2c2517ca32a1
kerollos <kerolloz@yahoo.com>
2019-05-06 19:00:27 +0200

    implement contest model

---
+++ src/models/contest.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+const contestSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50
+  },
+  setter: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User"
+  },
+  problems: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "Problem"
+  },
+  registeredUsers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    required: true,
+    ref: "User"
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true // number of minutes the contest will last
+  },
+  password: {
+    type: String,
+    default: null // no password by default (the contest is general i.e: available for all the users)
+  }
+});
+
+export const Contest = mongoose.model("Contest", contestSchema);
+
+export function validateContest(contest: any){
+  const schma = {
+    name: Joi.string().required().min(1).max(50),
+    setter: Joi.string().required().min(1),
+    problems: Joi.array().required().min(1).items(Joi.string()),
+    registeredUsers: Joi.array().required().min(1).items(Joi.string()),
+    startDate: Joi.date().required().min(Date.now()),
+    duration: Joi.number().required().min(1),
+    password: Joi.string().min(1)
+  }
+}
+
