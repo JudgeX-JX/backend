ad1868ba5dcd9788f2b0aea5b8a83697d5a5db6f
kerollos <kerolloz@yahoo.com>
2019-05-06 21:55:00 +0200

    implement user authentication

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import { Response, NextFunction } from 'express';
+import config from 'config';
+
+export async function authenticate(req: any, res: Response, next: NextFunction) {
+  const token = req.header('x-auth-token');
+
+  if (!token)
+    return res.status(401).json({ message: 'Access denied! No token provided.' });
+
+  try {
+    req.user = await jwt.verify(token, config.get('jwtPrivateKey'));
+    next();
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+
+}
