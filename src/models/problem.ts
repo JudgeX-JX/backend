6b54e6c51b054a62243a0b5f4794e954809d7903
kerollos <kerolloz@yahoo.com>
2019-05-06 19:00:48 +0200

    add missing fields in problem model

--- src/models/problem.ts
+++ src/models/problem.ts
@@ -15,6 +15,23 @@ const problemSchema = new mongoose.Schema({
     minlength: 1,
     maxlength: 50
   },
+  setter: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User"
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  inputs: {
+    type: [String],
+    required: true
+  },
+  outputs: {
+    type: [String],
+    required: true
+  },
   timeLimit: {
     type: Number,
     required: true,
@@ -34,7 +51,8 @@ const problemSchema = new mongoose.Schema({
     type: String,
     required: true,
     enum: [...enumToArray(Difficulty)]
-  }
+  },
+
 });
 
 export const Problem = mongoose.model("Problem", problemSchema);
@@ -42,9 +60,14 @@ export const Problem = mongoose.model("Problem", problemSchema);
 export function validateProblem(problem: any) {
   const schema = {
     name: Joi.string().required().min(1).max(50),
+    setter: Joi.string().required().min(1),
+    description: Joi.string().required().min(10),
+    inputs: Joi.array().required(),
+    outputs: Joi.array().required().length(problem.inputs.length),
     timeLimit: Joi.number().required().min(1).max(10),
     memoryLimit: Joi.number().required().min(5).max(500),
     tags: Joi.array(),
-    difficulty: Joi.string().required().valid([...enumToArray(Difficulty)])
-  }
+    difficulty: Joi.string().required().valid([...enumToArray(Difficulty)]),
+  };
+  return Joi.validate(problem, schema);
 }
