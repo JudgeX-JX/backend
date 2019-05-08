fb8d86824d3e1f7305bb805991e28edd519eccd3
kerollos <kerolloz@yahoo.com>
2019-05-08 08:32:04 +0200

    implement GET /submissions
    on submission controller

--- src/controllers/submissionController.ts
+++ src/controllers/submissionController.ts
@@ -1,14 +1,42 @@
 import { Submission, validateSubmission } from '../models/submission';
 import { Request, Response } from 'express';
+import axios from 'axios';
+
 
 // CRUD operations
 
 export function getAll(req: Request, res: Response) {
+  const options = {
+    page: parseInt(req.query.pageNumber) || 1,
+    limit: parseInt(req.query.pageSize) || 10,
+    populate: {
+      path: "user problem",
+      select: "name"
+    },
+    customLabels: {
+      docs: 'submissions'
+    }
+  };
+  Submission.paginate({}, options, (err, result) => {
+    if (err) return res.json({ message: err });
+    res.send(result);
+  });
 
 }
 
 export function getWithId(req: Request, res: Response) {
+  const submissionId = req.params.id;
 
+  Submission.findById(submissionId).populate({
+    path: "user problem",
+    select: "name"
+  }).then((result) => {
+    if (!result)
+      return res.status(404).json({ message: "No submission with the specified id: " + submissionId })
+    res.send(result);
+  }).catch((error) => {
+    res.status(404).send(error);
+  });
 }
 
 export function create(req: Request, res: Response) {
