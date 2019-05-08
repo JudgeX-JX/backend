6552db7f33a0354a1ae78fc7b7c84fd8763f05fa
kerollos <kerolloz@yahoo.com>
2019-05-08 04:12:23 +0200

    implement GET & POST for /problems route
    on problem controller

--- src/controllers/problemController.ts
+++ src/controllers/problemController.ts
@@ -4,14 +4,41 @@ import { Request, Response } from 'express';
 // CRUD operations
 
 export function getAll(req: Request, res: Response) {
-
+  const options = {
+    page: parseInt(req.query.pageNumber) || 1,
+    limit: parseInt(req.query.pageSize) || 10,
+    populate: {
+      path: "setter",
+      select: "name"
+    },
+    customLabels: {
+      docs: 'problems'
+    }
+  };
+  Problem.paginate({}, options, (err, result) => {
+    if (err) return res.json({ message: err });
+    res.send(result);
+  });
 }
 
 export function getWithId(req: Request, res: Response) {
+  const problemId = req.params.id;
 
+  Problem.findById(problemId).then((result) => {
+    res.send(result);
+  }).catch((error) => {
+    res.status(404).send(error);
+  });
 }
 
-export function create(req: Request, res: Response) {
+export function create(req: Request | any, res: Response) {
+  const { error } = validateProblem(req.body);
+  if (error) return res.status(422).json({ message: error.details[0].message });
+
+  const problem = new Problem(req.body);
+  problem.setter = req.user._id;
+  problem.save();
+  res.send(problem);
 
 }
 
