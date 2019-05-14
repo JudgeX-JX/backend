3f329b3a59937fd1919a484653b764988d36738f
Amr Salama <amr.salama@hamza.solutions>
2019-05-14 04:52:01 +0200

    add more essential express middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,29 @@
-import { Server } from 'http';
+import http from 'http';
 import clrs from 'colors';
 import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import routes from './routes';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+const logger =
+  app.get('env') === 'development'
+    ? morgan('dev')
+    : morgan('combined', {
+        skip: (req, res) => res.statusCode < 500
+      });
+
+app.use(logger);
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(cors());
 app.use(helmet());
 app.use(routes);
 
 export async function run() {
-  return new Promise<Server>((resolve, reject) => {
+  return new Promise<http.Server>((resolve, reject) => {
     const port = process.env.PORT || 5000;
     const server = app.listen(port);
 
