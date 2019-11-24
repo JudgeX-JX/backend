3c7e0263089aaa0978c519844687fae475e50990
kerollos <kerolloz@yahoo.com>
2019-11-24 05:51:12 +0200

    refactor create contest

--- src/controllers/contest/createContest.ts
+++ src/controllers/contest/createContest.ts
@@ -1,16 +1,18 @@
 import { Contest, validateContest, validateProblems } from '../../models/contest';
 import { Request, Response } from 'express';
 import _ from 'lodash';
+import APIResponse from '../../utils/APIResponse';
 
 export async function create(req: Request | any, res: Response) {
-    const { error } = validateContest(req.body);
-    if (error) return res.status(422).json({ message: error.details[0].message });
+  const { error } = validateContest(req.body);
+  if (error)
+    return APIResponse.UnprocessableEntity(res, error.message);
 
-    if (!validateProblems(req.body.problems))
-        return res.status(422).json({ message: 'Invalid problem id' });
+  if (!await validateProblems(req.body.problems))
+    return APIResponse.UnprocessableEntity(res, 'Invalid problem id');
 
-    const contest = new Contest(req.body);
-    contest.setter = req.user._id;
-    contest.save();
-    res.send(contest);
+  const contest = new Contest(req.body);
+  contest.setter = req.user._id;
+  contest.save();
+  res.send(contest);
 }
