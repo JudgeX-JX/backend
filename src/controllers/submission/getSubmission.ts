256c4d120ce45495a320bac2a30305209b3ea031
kerollos <kerolloz@yahoo.com>
2019-11-24 03:08:13 +0200

    refactor get submission

--- src/controllers/submission/getSubmission.ts
+++ src/controllers/submission/getSubmission.ts
@@ -1,22 +1,23 @@
 import { Submission } from '../../models/submission';
 import { Request, Response } from 'express';
-// CRUD operations
-export function getWithId(req: Request, res: Response) {
-    const submissionId = req.params.id;
+import APIResponse from '../../utils/APIResponse';
 
-    Submission.findById(submissionId)
-        .populate({
-            path: 'user problem',
-            select: 'name'
-        })
-        .then(result => {
-            if (!result)
-                return res.status(404).json({
-                    message: 'No submission with the specified id: ' + submissionId
-                });
-            res.send(result);
-        })
-        .catch(error => {
-            res.status(404).send(error);
-        });
+export async function getWithId(req: Request, res: Response) {
+  const submissionId = req.params.id;
+
+  try {
+    const submission = await Submission.findById(submissionId)
+      .populate({
+        path: 'user problem',
+        // select: 'name'
+      })
+    if (!submission) return APIResponse.NotFound(res, `No submission with id ${submissionId}`)
+
+    return APIResponse.Ok(res, submission);
+  }
+  catch (err) {
+    console.log(err);
+    return APIResponse.BadRequest(res, err);
+  }
+
 }
