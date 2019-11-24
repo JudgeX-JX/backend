12d3cfc3dc49f7720a2d14896b86a69452b42f71
kerollos <kerolloz@yahoo.com>
2019-11-24 12:01:50 +0200

    only get problems that their contest has started

--- src/controllers/problem/getAllProblems.ts
+++ src/controllers/problem/getAllProblems.ts
@@ -1,21 +1,48 @@
 import { Problem } from '../../models/problem';
 import { Request, Response } from 'express';
+import { Contest } from '../../models/contest';
+import APIResponse from '../../utils/APIResponse';
 
 
-export function getAll(req: Request, res: Response) {
-    const options = {
-        page: parseInt(req.query.pageNumber) || 1,
-        limit: parseInt(req.query.pageSize) || 10,
-        populate: {
-            path: 'setter',
-            select: 'name'
-        },
-        customLabels: {
-            docs: 'problems'
-        }
-    };
-    Problem.paginate({}, options, (err, result) => {
-        if (err) return res.json({ message: err });
-        res.send(result);
-    });
+export async function getAll(req: Request, res: Response) {
+  const options = {
+    page: parseInt(req.query.pageNumber) || 1,
+    limit: parseInt(req.query.pageSize) || 10,
+    customLabels: {
+      docs: 'problems'
+    }
+  };
+  // const problems = await Contest.aggregate({
+  //   startDate: { $lt: new Date() }
+  // }).select('problems').populate('problems')
+
+  const problems = await Contest.aggregate([
+    {
+      $match: {
+        startDate: { $lt: new Date() }
+      }
+    },
+    {
+      $lookup: {
+        from: 'problems',
+        localField: 'problems',
+        foreignField: '_id',
+        as: 'problems',
+      },
+    },
+    {
+      $unwind: '$problems',
+    },
+    {
+      $replaceRoot: {
+        newRoot: '$problems',
+      },
+    },
+  ]);
+
+  return APIResponse.Ok(res, problems);
+  Problem.paginate({}, options, (err, result) => {
+    if (err) return res.json({ message: err });
+    res.send(result);
+  });
 }
