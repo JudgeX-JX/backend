412669f9f0aacd3bf945aa4aef153259108122c0
Kerollos Magdy <kerolloz@yahoo.com>
2020-03-23 04:50:49 +0200

    fix update contest

--- src/controllers/contest/updateContest.ts
+++ src/controllers/contest/updateContest.ts
@@ -1,6 +1,5 @@
 import { Contest, validateContest, validProblemIDs } from '../../models/contest';
 import { Request, Response } from 'express';
-import _ from 'lodash';
 import APIResponse from '../../utils/APIResponse';
 
 
@@ -13,28 +12,12 @@ export async function updateWithId(req: Request, res: Response): Promise<Respons
     return APIResponse.NotFound(res, 'No contest with id ' + contestId)
   }
 
-  // if the problems are changed and they are NOT valid
-  const areNotValid =
-    req.body.problems && !(await validProblemIDs(req.body.problems));
-
-  if (areNotValid) { return APIResponse.UnprocessableEntity(res, 'Invalid problem id'); }
-
-  // fill req.body with the required fields for Joi validation
-  req.body = {
-    ..._.pick(contest, ['name', 'password', 'startDate', 'duration']),
-    problems: [
-      ...contest.problems.map((problemId: any) => problemId.toString())
-    ],
-    // problems is array of ObjectIDs (make them strings for Joi validation)
-    ...req.body
-  };
-
   const { error } = validateContest(req.body);
-  if (error) { return APIResponse.UnprocessableEntity(res, error.details[0].message); }
+  if (error) { return APIResponse.UnprocessableEntity(res, error.message); }
 
-  _.merge(contest, req.body);
-
-  contest.save();
+  if (!await validProblemIDs(req.body.problems)) {
+    return APIResponse.UnprocessableEntity(res, 'Invalid problem id');
+  }
+  await contest.set(req.body).save();
   return APIResponse.Ok(res, contest);
-
 }
