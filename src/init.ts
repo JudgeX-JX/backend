2cf2f571f316a2db2cbecc7b876c574815d5749b
kerollos <kerolloz@yahoo.com>
2020-03-31 04:36:16 +0200

    fix init script

--- src/init.ts
+++ src/init.ts
@@ -2,24 +2,24 @@ import {User, Roles} from './models/user';
 import {blue, red} from 'colors/safe';
 
 export default async (): Promise<void> => {
+  const password = process.env.ADMIN_PASSWORD || 'admin@admin';
+
   let user = await User.findOne({
+    email: 'admin@admin.com',
     fromInit: true,
   });
-
-  const generateRandomPassword = (): string =>
-    Math.random().toString(36).slice(-8);
 
   if (!user) {
     user = new User({
       name: 'admin',
       email: 'admin@admin.com',
-      password: generateRandomPassword(),
+      password,
       role: Roles[Roles.ADMIN],
       fromInit: true,
     });
     await user.save();
   }
   console.log(red('--- Created ADMIN ---'));
-  console.log(blue(`${user.email}\n${user.password}`));
+  console.log(blue(`${user.email}\n${password}`));
   console.log(red('---------------------'));
 };
