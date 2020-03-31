2cbe5ee9d4a5e59a0cb94b49639461675fbf5f8b
kerollos <kerolloz@yahoo.com>
2020-03-31 17:51:31 +0200

    add jsdoc to noId function

--- src/utils/noId.ts
+++ src/utils/noId.ts
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 
+/**
+ * Returns a string reporting no <model.name> with <id>
+ * @param model The model you want to use its name
+ * @param id The document id you want to use
+ */
 export default function (
-  document: mongoose.Model<mongoose.Document>,
+  model: mongoose.Model<mongoose.Document>,
   id: string,
 ): string {
-  return `No ${document.modelName.toUpperCase()} with id ${id}`;
+  return `No ${model.modelName.toUpperCase()} with id ${id}`;
 }
