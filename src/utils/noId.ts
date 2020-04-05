b14faf06624da93fb1149e9fd95331b5fed81b97
kerollos <kerolloz@yahoo.com>
2020-04-05 20:28:21 +0200

    refactor noId file

--- src/utils/noId.ts
+++ src/utils/noId.ts
@@ -1,13 +1,10 @@
-import mongoose from 'mongoose';
+import {Model, Document} from 'mongoose';
 
 /**
  * Returns a string reporting no <model.name> with <id>
  * @param model The model you want to use its name
  * @param id The document id you want to use
  */
-export default function (
-  model: mongoose.Model<mongoose.Document>,
-  id: string,
-): string {
+export default function (model: Model<Document>, id: string): string {
   return `No ${model.modelName.toUpperCase()} with id ${id}`;
 }
